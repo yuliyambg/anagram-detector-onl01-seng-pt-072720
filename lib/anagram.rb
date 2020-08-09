9d39475621c9b831db0cf5d3157d02627592baad
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-09 18:24:45 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word= word
+  end
+
+  def match(anagrams_array)
+    result = []
+    input = word.downcase.split("")
+    anagrams_array.each do |element|
+      anagram_word = element.downcase.split("") 
+      if anagram_word.sort == input.sort
+        result << element
+      end
+    end
+    result
+  end
+
+end

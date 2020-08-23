b94bcc63697d7e2f8f2a62dfcaa1e131e0bf237e
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-23 19:02:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,23 +2,23 @@
 
 
 
-class Anagram
-  attr_accessor :word
+# class Anagram
+#   attr_accessor :word
   
-  def initialize(word)
-    @word= word
-  end
+#   def initialize(word)
+#     @word= word
+#   end
 
-  def match(anagrams_array)
-    result = []
-    input = word.downcase.split("")
-    anagrams_array.each do |element|
-      anagram_word = element.downcase.split("") 
-      if anagram_word.sort == input.sort
-        result << element
-      end
-    end
-    result
-  end
+#   def match(anagrams_array)
+#     result = []
+#     input = word.downcase.split("")
+#     anagrams_array.each do |element|
+#       anagram_word = element.downcase.split("") 
+#       if anagram_word.sort == input.sort
+#         result << element
+#       end
+#     end
+#     result
+#   end
 
-end
+# end
